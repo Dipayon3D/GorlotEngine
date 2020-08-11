2836c4cfea94d73fcef6369217dbce6400c61467
sallie <saraaahinreallife@gmail.com>
2020-08-11 20:38:41 +0000

    Fixed program dispose resources

--- Engine/Core/THREE/Material.js
+++ Engine/Core/THREE/Material.js
@@ -2,6 +2,26 @@
 
 THREE.Material.prototype.nodes = {}
 
+THREE.Material.prototype.dispose = function() {
+    function disposeTexture(texture) {
+		if(texture !== undefined && texture !== null) {
+			texture.dispose()
+		}
+	}
+
+	this.dispatchEvent({type:"dispose"})
+
+	disposeTexture(this.map)
+	disposeTexture(this.bumpMap)
+	disposeTexture(this.normalMap)
+	disposeTexture(this.displacementMap)
+	disposeTexture(this.specularMap)
+	disposeTexture(this.emissiveMap)
+	disposeTexture(this.alphaMap)
+	disposeTexture(this.roughnessMap)
+	disposeTexture(this.metalnessMap)
+}
+
 THREE.Material.prototype.updateNodes = function(nodes) {
 	if (this.nodes !== undefined) {
 		this.nodes = {}
