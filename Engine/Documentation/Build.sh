4139534385d743c056caaba7f351dcdb07c41cf2
sallie <saraaahinreallife@gmail.com>
2020-08-22 23:07:23 +0000

    Some tests with YUIDoc

---
+++ Engine/Documentation/Build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+echo "Generating the docs using YUI"
+yuidoc ../Source/Runtime -o Build
+echo "Done"
--- Engine/Source/Runtime/Gorlot.js
+++ Engine/Source/Runtime/Gorlot.js
@@ -1,12 +1,37 @@
 "use strict"
 
+/**
+ * GORLOT global namespace
+ * @class GORLOT
+ */
 function GORLOT() {}
 
+/**
+ * Contains the engine name
+ * @property NAME
+ * @type "String"
+ * @default "Gorlot"
+ */
 GORLOT.NAME = "Gorlot"
+
+/**
+ * The current engine version
+ * @property VERSION
+ * @type "String"
+ */
 GORLOT.VERSION = "0.0.1-alpha"
+
+/**
+ * The date in which the current version was compiled
+ * @property TIMESTAMP
+ */
 GORLOT.TIMESTAMP = "Wed Aug 19, 22:06 PM"
 
-// Check if WebVR is Available
+/**
+ * Checks if virtual reality is available
+ * @method WebVRAvailable
+ * @return {Boolean} Returns true if available
+ */
 GORLOT.WebVRAvailable = function() {
     return navigator.getVRDisplays !== undefined
 }
